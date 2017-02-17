14930cc6b49d370b4d7fbe8875d4821d14a34965
freda nada <fredasnada@gmail.com>
2017-02-17 11:50:15 -0500

    setup

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,12 +1,12 @@
-// const SET_TEXT = 'SET_TEXT'
-//
-// export const write = text => ({ type: SET_TEXT, text})
-//
-// export default function reducer (state = {}, action) {
-//   switch (action.type) {
-//     case SET_TEXT:
-//        return action.text
-//     default:
-//        return state
-//   }
-// }
+const SET_TEXT = 'SET_TEXT'
+
+export const write = text => ({ type: SET_TEXT, text})
+
+export default function reducer (state = {}, action) {
+  switch (action.type) {
+    case SET_TEXT:
+       return action.text
+    default:
+       return state
+  }
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-// import { createStore } from 'redux'
-// import rootReducer from './root-reducer.js'
-//
-// export default createStore(rootReducer)
+import { createStore } from 'redux'
+import rootReducer from './root-reducer.js'
+
+export default createStore(rootReducer)
